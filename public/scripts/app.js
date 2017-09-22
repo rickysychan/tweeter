1f16ee4adbbd3649955d0e46c30c27adcc8833e9
Ricky Chan <rickysychan@gmail.com>
2017-09-22 19:49:12 +0000

    added the font awesome heart to replace the original heart, Now when clicked it will turn red!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,3 @@
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
  function renderTweets(tweets) {
   tweets.sort(function (a, b) {
     if(a.created_at === b.created_at) {
@@ -90,6 +43,14 @@ $(document).ready(function(){
   });
 });
 
+//this code first loads the tweets so the user can see them than it serializes the data
+// passed in from submitting the form which than it gets the value of. It validates
+// to see if anything is entered or if too much has been entered then if it passes it
+// makes a post requests to /tweets passing in the form data. routes from tweets witll
+// check to see if there is user data, if there isn't it will create a random user data
+// profie and send a 201 and save the data into the DB. back here when that has been done
+// it will load the tweets again with the updated tweets. that of fail and log the error
+
 $(document).ready(function() {
     $('#theForm textarea').keydown(function(event) {
         if (event.keyCode == 13) {
@@ -97,6 +58,12 @@ $(document).ready(function() {
             return false;
          }
     });
+});
+
+$(document).ready(function(){
+  $(document).on('click', '.fa-heart', function(event){
+    $(this).toggleClass("lblCountRed")
+  })
 });
 
 // this first loads the tweets so we can see the existing tweets than it has a on click
@@ -132,7 +99,7 @@ function createTweetElement(tweetData) {
   $footer.append($("<div>").addClass("icon-container"));
   $footer.append($("<img>").addClass("social-img").attr('src', '/images/flag.png'));
   $footer.append($("<img>").addClass("social-img").attr('src', '/images/re-tweet.png'));
-  $footer.append($("<img>").addClass("social-img").attr('src', '/images/heart.png'));
+  $footer.append($("<img>").addClass("social-img").addClass("like").innerHTML = '<i class="fa fa-heart" aria-hidden="true"></i>');
   $tweet.append($footer);
   return $tweet;
 }
